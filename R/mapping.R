64500fcba52a2569c644b6e9e4274dbb8d961a82
Jameel Alsalam <alsalam.jameel@epa.gov>
2020-05-23 22:31:24 -0400

    something funny with call wo input

--- R/mapping.R
+++ R/mapping.R
@@ -1,19 +1,36 @@
 
-#' Create a mapping that maps names to values
+#' Low Level Constructor for Key-Value Mappings
+#'
+#' @param keys from the domain
+#' @param values to which to map the keys
+#'
+#' Right now mapping uses a named vector lookup, so only character vectors can be keys.
+#' This will be generalized to vectors in the future.
 #'
 #' @import vctrs
 #' @import rlang
 #' @export
 #' @examples
-#' lookup <- new_mapper(c("m" = "Male", "f" = "Female", "u" = NA))
+#' lookup <- new_mapping(c("m", "f", "u"), c("Male", "Female", NA))
 #' x <- c("m", "f", "u", "f", "f", "m", "m")
 #' lookup(x)
-new_mapping <- function(keyvals) {
-  stopifnot(vec_size(names(keyvals)) == vec_size(keyvals))
+new_mapping <- function(keys, values) {
+  stopifnot(vec_size(keys) == vec_size(values))
 
-  new_function(
-    args = pairlist2(x=, kv=keyvals),
-    body = quote(unname(kv[x])),
-    env = caller_env()
+  keyvals <- setNames(values, keys)
+
+  structure(
+    new_function(
+      args = pairlist2(x=NULL, kv=keyvals),
+      body = quote(unname(kv[x])),
+      env = caller_env()
+    ),
+
+    class = c("mapping", "vctrs_vctr")
   )
+}
+
+#' @export
+vec_proxy.mapping <- function(x, ...) {
+  fn_fmls(x)[["kv"]]
 }
--- tests/testthat/test-construct-mapping.R
+++ tests/testthat/test-construct-mapping.R
@@ -2,7 +2,12 @@
 context("construct and apply a mapping")
 
 test_that("constructor works", {
-  lookup <- new_mapping(c("m" = "Male", "f" = "Female", "u" = NA))
+  lookup <- new_mapping(c("m", "f", "u"), c("Male", "Female", NA))
   x <- c("m", "f", "u", "f", "f", "m", "m")
   expect_equal(lookup(x), c("Male", "Female", NA, "Female", "Female", "Male", "Male"))
+})
+
+test_that("calling mapping without args returns zero-length result", {
+  lookup <- new_mapping(keys = c("1"), values = 2)
+  expect_equal(lookup(), numeric())
 })
